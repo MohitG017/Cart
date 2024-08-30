3393225cdf5eae7e49d9e981199b44d7e31ed872
MohitG017 <mohit@abcdeventures.com>
2024-08-30 14:55:13 +0530

    fix endpoint

--- db/database.go
+++ db/database.go
@@ -5,20 +5,20 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite" // Import SQLite dialect
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	// Update with your SQLite database file path
-	dsn := "gorm.db" // SQLite database file
+
+	dsn := "gorm.db"
 	db, err := gorm.Open("sqlite3", dsn)
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	// Auto migrate the models
+	
 	db.AutoMigrate(&models.User{}, &models.Cart{}, &models.Item{}, &models.Order{}, &models.CartItem{})
 
 	DB = db
